4e00fec1b5754b98f127a0f857b43c87ff89a207
Shanaka <shanakamunasingheplay@gmail.com>
2020-04-02 12:20:33 +0530

    done adding Stock and User models

---
+++ stock/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name + " , " + self.password
+
+
+class Stock(models.Model):
+    date = models.DateTimeField()
+    data = models.FloatField()
+
+    def __str__(self):
+        return str(self.data)
